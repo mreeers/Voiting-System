eb9c0c6a0c4f95e903cadeab4965fb1b8c40c797
Александр Ефанов <verpl01@yandex.ru>
2020-10-22 22:51:41 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yesCounter = 4;
+            int noCounter = 2;
+            int maybeCounter = 3;
+
+            int total = yesCounter + noCounter + maybeCounter;
+
+            var yesPercent = Math.Round(yesCounter * 100.0 / total, 2);
+            var noPercent = Math.Round(noCounter * 100.0 / total, 2);
+            var maybePercent = Math.Round(maybeCounter * 100.0 / total, 2);
+
+
+            var excess = Math.Round(100 - yesPercent - noPercent - maybePercent, 2);
+
+            Console.WriteLine($"Excess: {excess}");
+
+            if (yesCounter > noCounter)
+            {
+                if (yesCounter > maybeCounter)
+                {
+                    Console.WriteLine("Yes Won");
+                    yesPercent += excess;
+                }
+                else if (maybeCounter > yesCounter)
+                {
+                    Console.WriteLine($"Maybe won");
+                    maybePercent += excess;
+                }
+                else
+                {
+                    Console.WriteLine("DRAW");
+                }
+            }
+            else if (noCounter > maybeCounter)
+            {
+                if (noCounter > maybeCounter)
+                {
+                    Console.WriteLine("No Won");
+                    noPercent += excess;
+                }
+                else if (maybeCounter > noCounter)
+                {
+                    Console.WriteLine($"Maybe won");
+                }
+                else
+                {
+                    Console.WriteLine("DRAW");
+                }
+            }
+            else if (maybeCounter > yesCounter)
+            {
+                Console.WriteLine("DRAW");
+            }
+
+            Console.WriteLine($"Yes Counts: {yesCounter}, Percentage: {Math.Round(yesPercent, 2)}%");
+            Console.WriteLine($"No Counts: {noCounter}, Percentage: {Math.Round(noPercent, 2)}%");
+            Console.WriteLine($"No Counts: {maybeCounter}, Percentage: {Math.Round(maybePercent, 2)}%");
+            Console.WriteLine($"Total Percentage: {Math.Round(yesPercent + noPercent + maybePercent, 2)}");
+
+        }
+    }
+}
